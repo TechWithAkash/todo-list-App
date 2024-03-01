f5391b0f8b71f05ed28ced1836fc2e06631362ae
TechWithAkash <vishwakarmaakashav17@gmail.com>
2024-03-01 22:46:20 +0530

    22:46

--- src/Component/Header.js
+++ src/Component/Header.js
@@ -19,6 +19,9 @@ export default function Header(props) {
               <li className="nav-item">
                 <Link className="nav-link" to="/about">About</Link>
               </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/contact">Contact Us</Link>
+              </li>
             </ul>
             { props.searchBar ? <form className="d-flex" role="search">
               <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
--- src/Component/contact.js
+++ src/Component/contact.js
@@ -1,8 +1,18 @@
 import React from 'react'
-
+import './contact.css'
 const contact = () => {
   return (
-    <div>contact Page</div>
+    <div className="container">
+        <h1>Student Details</h1>
+        <ul>
+            <li>Name</li>
+            <li>Rollno</li>
+            <li>age</li>
+            <li>Department</li>
+            <li>Teachers</li>
+            <li>Speciality</li>
+        </ul>
+    </div>
   )
 }
 
