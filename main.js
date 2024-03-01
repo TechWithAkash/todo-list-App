a31ed694df15df6f2634520748f9e3924937f517
TechWithAkash <vishwakarmaakashav17@gmail.com>
2024-03-01 21:59:39 +0530

    +

--- main.js
+++ main.js
@@ -1,9 +1,17 @@
 
-const { app, BrowserWindow } = require('electron');
+// const { app, BrowserWindow } = require('electron');
 
-app.whenReady().then(()=>{
-   const window =  new BrowserWindow({
-        width:800,
-        height:600
-    });
-});
+// app.whenReady().then(()=>{
+//    const window =  new BrowserWindow({
+//         width:800,
+//         height:600
+//     });
+// });
+ const a  = 10;
+ 
+ if (a%2==0){
+    console.log(a," is an Even Number");
+ }
+ else{
+    console.log(a," is Odd Number");
+ }
